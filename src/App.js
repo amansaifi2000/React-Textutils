86587f1ce83a061ee0b6678f339a898735e594b2
Aman Ali Saifi <amansaifi8430@gmail.com>
2023-05-13 05:28:34 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,45 @@
+// import './App.css';
+// import About from './Components/About';
+import { useState } from 'react';
+import Alert from './Components/Alert';
+import Navbar from './Components/Navbar';
+import Textform from './Components/Textform';
+
+function App(){
+  const[mode, setMode] = useState("light")
+  const[alert,setAlert] = useState(null)
+
+const showAlert = (message, type) => {
+    setAlert({
+      msg : message,
+    type : type,
+  })
+  setTimeout(() => {
+    setAlert(null)
+  }, 1500);
+}
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = '#092f48'
+      showAlert("Darkmode has been enabled", "success")
+    }
+    else {
+    setMode('light')
+    document.body.style.backgroundColor = 'white'
+    showAlert("Lightmode has been enabled", "success")
+    }
+  }
+return (
+  <>
+{/* <Navbar title = "Try Textutils" about = 'About TextUtils'/> */}
+{/* <Navbar/> */}
+<Navbar title = "Try Textutils" mode={mode} toggleMode={toggleMode}/>
+<Alert alert = {alert}/>
+<div className = "container my-3">
+<Textform showAlert = {showAlert} heading="Enter Your Thoughts" mode = {mode}/>
+</div>
+</>
+  );
+ }
+export default App;
---
+++ src/Components/Textform.js
@@ -0,0 +1,103 @@
+import React,{useState} from 'react'
+
+export default function Textform(props) {
+    const handleUpclick = () =>
+    {
+        if (text.length === 0) {
+            setText("");
+            props.showAlert("Please Enter Some Text To Convert Into Uppercase", "warning");
+          } else {
+        let nextText = text.toUpperCase();
+        setText(nextText)
+        props.showAlert("Converted Into Uppercase!", "success")
+    }
+}
+        const handleLoclick = () =>
+        {   
+        if (text.length === 0) {
+        setText("");
+        props.showAlert("Please Enter Some Text To Convert Into Lowercase", "warning");
+          } else {
+            let nextText1 = text.toLowerCase();
+            setText(nextText1)
+            props.showAlert("Converted Into Lowercase!", "success")
+        }
+    }
+        const handleSeclick = () =>
+        {
+            if (text.length === 0) {
+                setText("");
+                props.showAlert("Please Enter Some Text", "warning");
+              } else {
+                let sentenceCase = text[0].toUpperCase() + text.slice(1).toLowerCase();
+                setText(sentenceCase);
+                props.showAlert("Converted Into Sentence Case!", "success");
+              }     
+        }
+        const handleDeclick = () =>
+        {
+            let clearText = ''
+                setText(clearText)
+                props.showAlert("Your Text Has Been Cleared!", "success")
+        }
+        const handleCaclick = () =>
+        {
+    //         if (text.length === 0) {
+    //             setText("");
+    //             props.showAlert("Please Enter Some Text!", "warning");
+    //           } else {
+    //             const sentenceCase1 = (text) => text[0].toUpperCase() + text.slice(1).toLowerCase();
+    //             const words = text.trim().text.split(/\s+/).map(sentenceCase1);
+    //             const capitalizedSentence = words.join(' ');
+    //             setText(capitalizedSentence);
+    //             props.showAlert("Converted Into Capitalized Text!", "success");
+    //           }
+    if (text.trim().length === 0) {
+      setText("");
+      props.showAlert("Please Enter Some Text!", "warning");
+    } else {
+      const sentenceCase1 = (text) => text[0].toUpperCase() + text.slice(1).toLowerCase();
+      const words = text.trim().split(/\s+/).map(sentenceCase1);
+      const capitalizedSentence = words.join(' ');
+      setText(capitalizedSentence);
+      props.showAlert("Converted Into Capitalized Text!", "success");
+    }
+    }
+        const handleExclick = () =>
+        {
+            let removeSpaces = text.split(/[ ]+/)
+            setText(removeSpaces.join(" "))   
+            props.showAlert("Spaces Has Been Removed!", "success")
+        }
+    const handleonChange = (event) =>
+    {
+        // let nextText = text.toUpperCase;
+        setText(event.target.value)
+    }
+    
+    const [text,setText] = useState("")    
+  return (
+    <>
+    <div className= "Container" style = {{color:props.mode === 'dark'?'white':'#092f48'}} >
+<div className="mb-3">
+<h1>{props.heading}</h1>
+<textarea className="form-control" value={text}onChange={handleonChange} style = {{backgroundColor: props.mode 
+    === 'dark'?'grey':'white', color:props.mode === 'dark'?'white':'#092f48'}}id="mybox" rows="8"></textarea>
+</div>
+<button className="btn btn-primary mx-1" onClick={handleUpclick}>Convert to Uppercase</button>
+<button className="btn btn-primary mx-1" onClick={handleLoclick}>Convert to Lowercase</button>
+<button className="btn btn-primary mx-1" onClick={handleSeclick}>Sentence Case</button>
+<button className="btn btn-primary mx-1" onClick={handleCaclick}>Capital Letters</button>
+<button className="btn btn-primary mx-1" onClick={handleExclick}>Remove Extra Spaces</button>
+<button className="btn btn-primary mx-1" onClick={handleDeclick}>Clear Text</button>
+</div>
+<div className = "Container my-3" style = {{color : props.mode === 'dark'?'white':'#092f48'}}>
+<h2>Text Summary</h2>
+<p>{text.trim().length === 0 ? 0 : text.trim().split(" ").length} words and {text.length} characters</p>
+{ text.length > 0 && <p>{(text.length / 200).toFixed(1)} Minutes to read</p> }
+<h2>Preview Text</h2>
+<p>{text.length>0?text:"Enter something in the textbox to preview it here"}</p>
+    </div>
+</>
+  )
+}
