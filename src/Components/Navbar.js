4ccf114a863dfde268d4b727a36701b9aee177f3
Aman Ali Saifi <amansaifi8430@gmail.com>
2023-05-16 16:26:31 +0530

    deleted app.js and components folder

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,19 +10,7 @@ export default function Navbar(props) {
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
-          </li>
-          <li className="nav-item">
-            {/* <a className="nav-link" href="#">{props.about}</a> */}
-          </li>
-          <li className="nav-item dropdown">
-          </li>
         </ul>
-        {/* {<form className="d-flex" role="search">
-          {<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>}
-          {<button className="btn btn-success" type="submit">Search</button> }
-        </form>} */}
       <div className={`form-check form-switch text-${props.mode === 'light'?'dark':'light'}`}>
      <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
      <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
